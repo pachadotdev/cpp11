93655d67fac7b70dc275ae683ca23b6ebf71222f
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2025-02-01 15:09:43 -0500

    save before moving to separate code

--- inst/include/cpp11/dmatrix.hpp
+++ inst/include/cpp11/dmatrix.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include "cpp11/R.hpp"
-#include "cpp11/matrix.hpp"  // Ensure this header is included to use by_column
+#include "cpp11/matrix.hpp"
 #include "cpp11/r_vector.hpp"
 #include "cpp11/sexp.hpp"
-#include <iostream>
 
 ////////////////////////////////////////////////////////////////////////////////
 // dgeMatrix: Real matrices in general storage mode
@@ -23,11 +22,8 @@ class dge_matrix {
   dge_matrix(SEXP data) {
     SEXP x_slot = R_do_slot(data, Rf_install("x"));
 
-    // get dimensions
-
     // nrow_ = Rf_nrows(data);
     // ncol_ = Rf_ncols(data);
-
     // use attr instead (Matrix != matrix class)
     SEXP dim = R_do_slot(data, Rf_install("Dim"));
     nrow_ = INTEGER(dim)[0];
@@ -41,12 +37,6 @@ class dge_matrix {
     for (int i = 0; i < Rf_length(x_slot); ++i) {
       vector_[i] = REAL(x_slot)[i];
     }
-
-    SEXP dims = PROTECT(Rf_allocVector(INTSXP, 2));
-    INTEGER(dims)[0] = nrow_;
-    INTEGER(dims)[1] = ncol_;
-    vector_.attr(R_DimSymbol) = dims;
-    UNPROTECT(1);
   }
 
   int nrow() const { return nrow_; }
@@ -81,7 +71,6 @@ class dsy_matrix {
   dsy_matrix(SEXP data) {
     SEXP x_slot = R_do_slot(data, Rf_install("x"));
 
-    // use attr instead (Matrix != matrix class)
     SEXP dim = R_do_slot(data, Rf_install("Dim"));
     nrow_ = INTEGER(dim)[0];
     ncol_ = INTEGER(dim)[1];
@@ -99,12 +88,6 @@ class dsy_matrix {
     for (int i = 0; i < Rf_length(x_slot); ++i) {
       vector_[i] = REAL(x_slot)[i];
     }
-
-    SEXP dims = PROTECT(Rf_allocVector(INTSXP, 2));
-    INTEGER(dims)[0] = nrow_;
-    INTEGER(dims)[1] = ncol_;
-    vector_.attr(R_DimSymbol) = dims;
-    UNPROTECT(1);
   }
 
   int nrow() const { return nrow_; }
@@ -156,7 +139,6 @@ class dsp_matrix {
   dsp_matrix(SEXP data) {
     SEXP x_slot = R_do_slot(data, Rf_install("x"));
 
-    // use attr instead (Matrix != matrix class)
     SEXP dim = R_do_slot(data, Rf_install("Dim"));
     nrow_ = INTEGER(dim)[0];
     ncol_ = INTEGER(dim)[1];
@@ -165,26 +147,18 @@ class dsp_matrix {
       throw std::runtime_error("Invalid matrix dimensions");
     }
 
-    // Check if the matrix is upper or lower triangular
     SEXP uplo = R_do_slot(data, Rf_install("uplo"));
     is_upper_ = (Rf_asChar(uplo) == Rf_mkChar("U"));
 
-    // Ensure the length of x_slot matches the expected length for packed storage
     if (Rf_length(x_slot) != (nrow_ * (nrow_ + 1)) / 2) {
       throw std::runtime_error("Invalid length of x slot for packed storage");
     }
 
     vector_ = writable::r_vector<double>(x_slot);
-
-    SEXP dims = PROTECT(Rf_allocVector(INTSXP, 2));
-    INTEGER(dims)[0] = nrow_;
-    INTEGER(dims)[1] = ncol_;
-    vector_.attr(R_DimSymbol) = dims;
-    UNPROTECT(1);
   }
 
   int nrow() const { return nrow_; }
-  int ncol() const { return ncol_; }
+  int ncol() const { return nrow_; }
 
   double operator()(int row, int col) const {
     if (is_upper_) {
@@ -199,14 +173,6 @@ class dsp_matrix {
       } else {
         return vector_[col * (col + 1) / 2 + row];
       }
-    }
-  }
-
-  double get(int row, int col) const {
-    if (row <= col) {
-      return operator()(row, col);
-    } else {
-      return operator()(col, row);
     }
   }
 };
